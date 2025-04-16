465740ab7995a0f50e02eea0aa3a794db609dea7
artjom-webcode <artjom.webcode@gmail.com>
2025-04-16 19:47:49 +0300

    css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,135 +14,132 @@ import { useLocalStorageState } from "./UseLocalStorageState";
 export const KEY = "a4169e6f";
 
 export default function App() {
-	const [query, setQuery] = useState("");
-	const [movies, setMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState("");
-	const [selectedId, setSelectedId] = useState(null);
-	const [wished, setWished] = useLocalStorageState(
-		[
-			{
-				imdbID: "tt1375666",
-				Title: "Inception",
-				Year: "2010",
-				Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-			},
-			{
-				imdbID: "tt0133093",
-				Title: "The Matrix",
-				Year: "1999",
-				Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-			},
-			{
-				imdbID: "tt6751668",
-				Title: "Parasite",
-				Year: "2019",
-				Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-			},
-		],
-		"watched"
-	);
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+  const [wished, setWished] = useLocalStorageState(
+    [
+      {
+        imdbID: "tt1375666",
+        Title: "Inception",
+        Year: "2010",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+      },
+      {
+        imdbID: "tt0133093",
+        Title: "The Matrix",
+        Year: "1999",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+      },
+      {
+        imdbID: "tt6751668",
+        Title: "Parasite",
+        Year: "2019",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+      },
+    ],
+    "watched"
+  );
 
-	function handleAddToWishedList(movie) {
-		setWished(function (wished) {
-			console.dir(wished);
-			return [...wished, movie];
-		});
-		setQuery("");
-	}
+  function handleAddToWishedList(movie) {
+    setWished(function (wished) {
+      console.dir(wished);
+      return [...wished, movie];
+    });
+    setQuery("");
+  }
 
-	function handleSelectedMovie(id) {
-		setSelectedId((selectedId) => (id === selectedId ? null : id));
-	}
+  function handleSelectedMovie(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
 
-	function handleCloseMovie() {
-		setSelectedId(null);
-	}
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
 
-	function handleDeleteWatched(id) {
-		setWished((wished) => wished.filter((movie) => movie.imdbID !== id));
-	}
+  function handleDeleteWatched(id) {
+    setWished((wished) => wished.filter((movie) => movie.imdbID !== id));
+  }
 
-	useEffect(
-		function () {
-			const controller = new AbortController();
-			async function fetshMovies() {
-				try {
-					setIsLoading(true);
-					setError("");
-					const res = await fetch(
-						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-						{
-							signal: controller.signal,
-						}
-					);
-					if (!res.ok)
-						throw new Error(
-							"Something went wrong with fetching movies!!!!!"
-						);
+  useEffect(
+    function () {
+      const controller = new AbortController();
+      async function fetshMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {
+            signal: controller.signal,
+          });
+          if (!res.ok) throw new Error("Something went wrong with fetching movies!!!!!");
 
-					const data = await res.json();
-					if (data.Response === "False") {
-						throw new Error("Movie not found");
-					}
+          const data = await res.json();
+          if (data.Response === "False") {
+            throw new Error("Movie not found");
+          }
 
-					setMovies(data.Search);
-					setError("");
-				} catch (err) {
-					if (err.name !== "AbortError") {
-						console.log(err.message);
-						setError(err.message);
-					}
-				} finally {
-					setIsLoading(false);
-				}
-			}
-			if (query.length < 3) {
-				setMovies([]);
-				setError("");
-				return;
-			}
-			fetshMovies();
-			return function () {
-				controller.abort();
-			};
-		},
-		[query]
-	);
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      fetshMovies();
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
 
-	return (
-		<>
-			<NavBar>
-				<Logo />
-				<div className="search-box">
-					<Search query={query} setQuery={setQuery} />
-					{isLoading && <Loader />}
-					{!isLoading && !error && (
-						<FindedMovieList
-							wished={wished}
-							onAddToWishedList={handleAddToWishedList}
-							movies={movies}
-						/>
-					)}
-					{error && <ErrorMessage message={error} />}
-				</div>
-				<NamResults movies={movies} />
-			</NavBar>
+  return (
+    <>
+      <NavBar>
+        <Logo />
+        <div className="search-box">
+          <Search query={query} setQuery={setQuery} />
+          {isLoading && <Loader />}
+          {!isLoading && !error && (
+            <FindedMovieList
+              wished={wished}
+              onAddToWishedList={handleAddToWishedList}
+              movies={movies}
+            />
+          )}
+          {error && <ErrorMessage message={error} />}
+        </div>
+        <NamResults movies={movies} />
+      </NavBar>
 
-			<Main>
-				<WatchlistList
-					onDeleteWatched={handleDeleteWatched}
-					onSelectMovie={handleSelectedMovie}
-					wished={wished}
-				/>
-				{selectedId && (
-					<MovieDetails
-						isLoading={isLoading}
-						selectedId={selectedId}
-						onCloseMovie={handleCloseMovie}
-					/>
-				)}
-			</Main>
-		</>
-	);
+      <Main>
+        <WatchlistList
+          onDeleteWatched={handleDeleteWatched}
+          onSelectMovie={handleSelectedMovie}
+          wished={wished}
+        />
+        {selectedId && (
+          <MovieDetails
+            isLoading={isLoading}
+            selectedId={selectedId}
+            onCloseMovie={handleCloseMovie}
+          />
+        )}
+      </Main>
+    </>
+  );
 }
--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -11,7 +11,7 @@ export function MovieDetails({ onCloseMovie, selectedId }) {
     function () {
       async function getMovieDetails() {
         setIsLoading(true);
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
         const data = await res.json();
         console.log(data);
         setMovie(data);
